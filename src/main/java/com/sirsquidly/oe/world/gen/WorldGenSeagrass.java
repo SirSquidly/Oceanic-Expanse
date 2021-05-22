0097f47efa2a2309fd61d79a12b41c5fa4bcd880
Sir Squidly <sir.squildy.le.third@gmail.com>
2021-05-22 12:17:39 -0700

    Committing everything

--- src/main/java/com/sirsquidly/oe/world/gen/WorldGenSeagrass.java
+++ src/main/java/com/sirsquidly/oe/world/gen/WorldGenSeagrass.java
@@ -4,13 +4,22 @@ import java.util.Random;
 
 import com.sirsquidly.oe.init.OEBlocks;
 
+import net.minecraft.block.BlockTallGrass;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenerator;
 
 public class WorldGenSeagrass extends WorldGenerator
 {
+	private final IBlockState tallGrassState;
+
+    public WorldGenSeagrass(BlockTallGrass.EnumType p_i45629_1_)
+    {
+        this.tallGrassState = Blocks.TALLGRASS.getDefaultState().withProperty(BlockTallGrass.TYPE, p_i45629_1_);
+    }
+    
     public boolean generate(World worldIn, Random rand, BlockPos position)
     {
         for (IBlockState iblockstate = worldIn.getBlockState(position); (iblockstate.getBlock().isAir(iblockstate, worldIn, position) || iblockstate.getBlock().isLeaves(iblockstate, worldIn, position)) && position.getY() > 0; iblockstate = worldIn.getBlockState(position))
@@ -22,7 +31,7 @@ public class WorldGenSeagrass extends WorldGenerator
         {
             BlockPos blockpos = position.add(rand.nextInt(8) - rand.nextInt(8), rand.nextInt(4) - rand.nextInt(4), rand.nextInt(8) - rand.nextInt(8));
 
-            if (worldIn.isAirBlock(blockpos) && OEBlocks.SEAGRASS.canPlaceBlockAt(worldIn, blockpos))
+            if (worldIn.getBlockState(blockpos).getBlock() == Blocks.WATER && OEBlocks.SEAGRASS.canPlaceBlockAt(worldIn, blockpos))
             {
             	worldIn.setBlockState(blockpos, OEBlocks.SEAGRASS.getDefaultState());
             }
