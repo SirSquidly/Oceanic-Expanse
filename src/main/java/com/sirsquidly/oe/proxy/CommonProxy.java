322699184f0d481e6156838d062c6b4e1f73640b
Sir Squidly <sir.squildy.le.third@gmail.com>
2021-06-03 21:34:29 -0700

    Tweaked Seagrass generation and the generator registries

--- src/main/java/com/sirsquidly/oe/proxy/CommonProxy.java
+++ src/main/java/com/sirsquidly/oe/proxy/CommonProxy.java
@@ -36,21 +36,20 @@ public class CommonProxy
     
     public static void registerWorldGen()
 	{
-    	GameRegistry.registerWorldGenerator(new GeneratorCoconutTree(3, 8, Biomes.BEACH), 0);
-    	GameRegistry.registerWorldGenerator(new WorldGenSeaOats(2, 3, 25, Biomes.BEACH), 0);
     	GameRegistry.registerWorldGenerator(new WorldGenShoreRock(1, 30, 3, false, Biomes.BEACH), 0);
     	GameRegistry.registerWorldGenerator(new WorldGenShoreRock(1, 15, 6, true, Biomes.STONE_BEACH), 0);
+    	GameRegistry.registerWorldGenerator(new WorldGenTidePools(2, 30, Biomes.BEACH), 0);
     	
-    	GameRegistry.registerWorldGenerator(new WorldGenKelpForest(2,40,4,48, BiomeDictionary.getBiomes(Type.OCEAN).toArray(new Biome[0])), 0);
+    	GameRegistry.registerWorldGenerator(new GeneratorCoconutTree(3, 8, Biomes.BEACH), 0);
+    	GameRegistry.registerWorldGenerator(new WorldGenSeaOats(2, 3, 25, Biomes.BEACH), 0);
     	
-    	//GameRegistry.registerWorldGenerator(new WorldGenKelp(10,10,48, BiomeDictionary.getBiomes(Type.OCEAN).toArray(new Biome[0])), 0);
-    	
+    	GameRegistry.registerWorldGenerator(new WorldGenKelpForest(BiomeDictionary.getBiomes(Type.OCEAN).toArray(new Biome[0])), 0);
+
     	GameRegistry.registerWorldGenerator(new WorldGenSeagrass(2, 6, 32, 0.4, true, BiomeDictionary.getBiomes(Type.OCEAN).toArray(new Biome[0])), 0);
     	
     	GameRegistry.registerWorldGenerator(new WorldGenSeagrass(2, 2, 48, 0.4, false, Biomes.RIVER), 0);
     	GameRegistry.registerWorldGenerator(new WorldGenSeagrass(6, 2, 48, 0.3, false, Biomes.OCEAN), 0);
     	GameRegistry.registerWorldGenerator(new WorldGenSeagrass(6, 2, 64, 0.8, false, Biomes.DEEP_OCEAN), 0);
-    	//GameRegistry.registerWorldGenerator(new WorldGenSeagrass(10, 2, 48, 0.3, false, OEBiomes.TIDE_POOL), 0);
     	GameRegistry.registerWorldGenerator(new WorldGenSeagrass(2, 2, 48, 0.6, false, BiomeDictionary.getBiomes(Type.SWAMP).toArray(new Biome[0])), 0);
 	}
 }
