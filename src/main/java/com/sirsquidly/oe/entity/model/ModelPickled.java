02f86b11a9ca81b100cd618e344636b10003a015
Sir Squidly <sir.squildy.le.third@gmail.com>
2022-03-26 16:40:56 -0700

    Tweaked/Fixed Pickled rendering to show held items, added Drowned Trident animations, and properly condensed Pickled model.

--- src/main/java/com/sirsquidly/oe/entity/model/ModelPickled.java
+++ src/main/java/com/sirsquidly/oe/entity/model/ModelPickled.java
@@ -1,14 +1,15 @@
 package com.sirsquidly.oe.entity.model;
 
-import net.minecraft.client.model.ModelBase;
-import net.minecraft.client.model.ModelBiped;
+import com.sirsquidly.oe.entity.EntityPickled;
+import com.sirsquidly.oe.init.OEItems;
+
 import net.minecraft.client.model.ModelRenderer;
-import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.model.ModelZombie;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.item.Item;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.EnumHandSide;
-import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -17,14 +18,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
  * Created using Tabula 7.1.0
  */
 @SideOnly(Side.CLIENT)
-public class ModelPickled extends ModelBiped {
-	public ModelRenderer bipedRightArm;
-    public ModelRenderer bipedRightLeg;
-    public ModelRenderer bipedHead;
-    public ModelRenderer bipedBody;
-    public ModelRenderer bipedLeftArm;
-    public ModelRenderer bipedLeftLeg;
-    public ModelRenderer bipedHeadwear;
+public class ModelPickled extends ModelZombie {;
     public ModelRenderer rightArmIntest1;
     public ModelRenderer rightArmIntest2;
     public ModelRenderer headIntest1;
@@ -33,6 +27,7 @@ public class ModelPickled extends ModelBiped {
     public ModelRenderer leftArmIntest2;
 
     public ModelPickled() {
+    	super();
     	this.textureWidth = 64;
         this.textureHeight = 64;
         this.bipedRightArm = new ModelRenderer(this, 40, 16);
@@ -92,278 +87,37 @@ public class ModelPickled extends ModelBiped {
     @Override
     public void render(Entity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale)
     {
+    	super.render(entityIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
+    	
         this.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale, entityIn);
-        GlStateManager.pushMatrix();
-
-        if (this.isChild)
-        {
-            GlStateManager.scale(0.75F, 0.75F, 0.75F);
-            GlStateManager.translate(0.0F, 16.0F * scale, 0.0F);
-            this.bipedHeadwear.render(scale);
-            this.bipedHead.render(scale);
-            GlStateManager.popMatrix();
-            GlStateManager.pushMatrix();
-            GlStateManager.scale(0.5F, 0.5F, 0.5F);
-            GlStateManager.translate(0.0F, 24.0F * scale, 0.0F);
-            this.bipedBody.render(scale);
-            this.bipedRightArm.render(scale);
-            this.bipedLeftArm.render(scale);
-            this.bipedRightLeg.render(scale);
-            this.bipedLeftLeg.render(scale);
-        }
-        else
-        {
-            if (entityIn.isSneaking())
-            {
-                GlStateManager.translate(0.0F, 0.2F, 0.0F);
-            }
-
-            this.bipedHead.render(scale);
-            this.bipedHeadwear.render(scale);
-            this.bipedBody.render(scale);
-            this.bipedRightArm.render(scale);
-            this.bipedLeftArm.render(scale);
-            this.bipedRightLeg.render(scale);
-            this.bipedLeftLeg.render(scale);
-        }
-
-        GlStateManager.popMatrix();
     }
 
-    /**
-     * This is a helper function from Tabula to set the rotation of model parts
-     */
-    public void setRotateAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+    public void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, Entity entityIn)
+    {
+		super.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor, entityIn);
+		
+		EntityPickled pickled = (EntityPickled) entityIn;
+		boolean flag = entityIn instanceof EntityZombie && ((EntityZombie)entityIn).isArmsRaised();
+		
+		Item mainItem = pickled.getHeldItem(EnumHand.MAIN_HAND).getItem();
+		
+		if (mainItem == OEItems.TRIDENT_ORIG && flag)
+		{
+			if (pickled.getPrimaryHand() == EnumHandSide.RIGHT)
+            {
+        		this.bipedRightArm.rotateAngleX = -4.0F;
+            }
+            if (pickled.getPrimaryHand() == EnumHandSide.LEFT)
+            {
+            	this.bipedLeftArm.rotateAngleX = 4.0F;
+            }
+		}
+    }
+    
+    public void setRotateAngle(ModelRenderer modelRenderer, float x, float y, float z)
+    {
         modelRenderer.rotateAngleX = x;
         modelRenderer.rotateAngleY = y;
         modelRenderer.rotateAngleZ = z;
-    }
-    
-    @SuppressWarnings("incomplete-switch")
-    public void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, Entity entityIn)
-    {
-        boolean flag = entityIn instanceof EntityLivingBase && ((EntityLivingBase)entityIn).getTicksElytraFlying() > 4;
-        this.bipedHead.rotateAngleY = netHeadYaw * 0.017453292F;
-
-        if (flag)
-        {
-            this.bipedHead.rotateAngleX = -((float)Math.PI / 4F);
-        }
-        else
-        {
-            this.bipedHead.rotateAngleX = headPitch * 0.017453292F;
-        }
-
-        this.bipedBody.rotateAngleY = 0.0F;
-        this.bipedRightArm.rotationPointZ = 0.0F;
-        this.bipedRightArm.rotationPointX = -5.0F;
-        this.bipedLeftArm.rotationPointZ = 0.0F;
-        this.bipedLeftArm.rotationPointX = 5.0F;
-        float f = 1.0F;
-
-        if (flag)
-        {
-            f = (float)(entityIn.motionX * entityIn.motionX + entityIn.motionY * entityIn.motionY + entityIn.motionZ * entityIn.motionZ);
-            f = f / 0.2F;
-            f = f * f * f;
-        }
-
-        if (f < 1.0F)
-        {
-            f = 1.0F;
-        }
-
-        this.bipedRightArm.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 2.0F * limbSwingAmount * 0.5F / f;
-        this.bipedLeftArm.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 2.0F * limbSwingAmount * 0.5F / f;
-        this.bipedRightArm.rotateAngleZ = 0.0F;
-        this.bipedLeftArm.rotateAngleZ = 0.0F;
-        this.bipedRightLeg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount / f;
-        this.bipedLeftLeg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount / f;
-        this.bipedRightLeg.rotateAngleY = 0.0F;
-        this.bipedLeftLeg.rotateAngleY = 0.0F;
-        this.bipedRightLeg.rotateAngleZ = 0.0F;
-        this.bipedLeftLeg.rotateAngleZ = 0.0F;
-
-        if (this.isRiding)
-        {
-            this.bipedRightArm.rotateAngleX += -((float)Math.PI / 5F);
-            this.bipedLeftArm.rotateAngleX += -((float)Math.PI / 5F);
-            this.bipedRightLeg.rotateAngleX = -1.4137167F;
-            this.bipedRightLeg.rotateAngleY = ((float)Math.PI / 10F);
-            this.bipedRightLeg.rotateAngleZ = 0.07853982F;
-            this.bipedLeftLeg.rotateAngleX = -1.4137167F;
-            this.bipedLeftLeg.rotateAngleY = -((float)Math.PI / 10F);
-            this.bipedLeftLeg.rotateAngleZ = -0.07853982F;
-        }
-
-        this.bipedRightArm.rotateAngleY = 0.0F;
-        this.bipedRightArm.rotateAngleZ = 0.0F;
-
-        switch (this.leftArmPose)
-        {
-            case EMPTY:
-                this.bipedLeftArm.rotateAngleY = 0.0F;
-                break;
-            case BLOCK:
-                this.bipedLeftArm.rotateAngleX = this.bipedLeftArm.rotateAngleX * 0.5F - 0.9424779F;
-                this.bipedLeftArm.rotateAngleY = 0.5235988F;
-                break;
-            case ITEM:
-                this.bipedLeftArm.rotateAngleX = this.bipedLeftArm.rotateAngleX * 0.5F - ((float)Math.PI / 10F);
-                this.bipedLeftArm.rotateAngleY = 0.0F;
-        }
-
-        switch (this.rightArmPose)
-        {
-            case EMPTY:
-                this.bipedRightArm.rotateAngleY = 0.0F;
-                break;
-            case BLOCK:
-                this.bipedRightArm.rotateAngleX = this.bipedRightArm.rotateAngleX * 0.5F - 0.9424779F;
-                this.bipedRightArm.rotateAngleY = -0.5235988F;
-                break;
-            case ITEM:
-                this.bipedRightArm.rotateAngleX = this.bipedRightArm.rotateAngleX * 0.5F - ((float)Math.PI / 10F);
-                this.bipedRightArm.rotateAngleY = 0.0F;
-        }
-
-        if (this.swingProgress > 0.0F)
-        {
-            EnumHandSide enumhandside = this.getMainHand(entityIn);
-            ModelRenderer modelrenderer = this.getArmForSide(enumhandside);
-            float f1 = this.swingProgress;
-            this.bipedBody.rotateAngleY = MathHelper.sin(MathHelper.sqrt(f1) * ((float)Math.PI * 2F)) * 0.2F;
-
-            if (enumhandside == EnumHandSide.LEFT)
-            {
-                this.bipedBody.rotateAngleY *= -1.0F;
-            }
-
-            this.bipedRightArm.rotationPointZ = MathHelper.sin(this.bipedBody.rotateAngleY) * 5.0F;
-            this.bipedRightArm.rotationPointX = -MathHelper.cos(this.bipedBody.rotateAngleY) * 5.0F;
-            this.bipedLeftArm.rotationPointZ = -MathHelper.sin(this.bipedBody.rotateAngleY) * 5.0F;
-            this.bipedLeftArm.rotationPointX = MathHelper.cos(this.bipedBody.rotateAngleY) * 5.0F;
-            this.bipedRightArm.rotateAngleY += this.bipedBody.rotateAngleY;
-            this.bipedLeftArm.rotateAngleY += this.bipedBody.rotateAngleY;
-            this.bipedLeftArm.rotateAngleX += this.bipedBody.rotateAngleY;
-            f1 = 1.0F - this.swingProgress;
-            f1 = f1 * f1;
-            f1 = f1 * f1;
-            f1 = 1.0F - f1;
-            float f2 = MathHelper.sin(f1 * (float)Math.PI);
-            float f3 = MathHelper.sin(this.swingProgress * (float)Math.PI) * -(this.bipedHead.rotateAngleX - 0.7F) * 0.75F;
-            modelrenderer.rotateAngleX = (float)((double)modelrenderer.rotateAngleX - ((double)f2 * 1.2D + (double)f3));
-            modelrenderer.rotateAngleY += this.bipedBody.rotateAngleY * 2.0F;
-            modelrenderer.rotateAngleZ += MathHelper.sin(this.swingProgress * (float)Math.PI) * -0.4F;
-        }
-
-        if (this.isSneak)
-        {
-            this.bipedBody.rotateAngleX = 0.5F;
-            this.bipedRightArm.rotateAngleX += 0.4F;
-            this.bipedLeftArm.rotateAngleX += 0.4F;
-            this.bipedRightLeg.rotationPointZ = 4.0F;
-            this.bipedLeftLeg.rotationPointZ = 4.0F;
-            this.bipedRightLeg.rotationPointY = 9.0F;
-            this.bipedLeftLeg.rotationPointY = 9.0F;
-            this.bipedHead.rotationPointY = 1.0F;
-        }
-        else
-        {
-            this.bipedBody.rotateAngleX = 0.0F;
-            this.bipedRightLeg.rotationPointZ = 0.1F;
-            this.bipedLeftLeg.rotationPointZ = 0.1F;
-            this.bipedRightLeg.rotationPointY = 12.0F;
-            this.bipedLeftLeg.rotationPointY = 12.0F;
-            this.bipedHead.rotationPointY = 0.0F;
-        }
-
-        this.bipedRightArm.rotateAngleZ += MathHelper.cos(ageInTicks * 0.09F) * 0.05F + 0.05F;
-        this.bipedLeftArm.rotateAngleZ -= MathHelper.cos(ageInTicks * 0.09F) * 0.05F + 0.05F;
-        this.bipedRightArm.rotateAngleX += MathHelper.sin(ageInTicks * 0.067F) * 0.05F;
-        this.bipedLeftArm.rotateAngleX -= MathHelper.sin(ageInTicks * 0.067F) * 0.05F;
-
-        if (this.rightArmPose == ModelBiped.ArmPose.BOW_AND_ARROW)
-        {
-            this.bipedRightArm.rotateAngleY = -0.1F + this.bipedHead.rotateAngleY;
-            this.bipedLeftArm.rotateAngleY = 0.1F + this.bipedHead.rotateAngleY + 0.4F;
-            this.bipedRightArm.rotateAngleX = -((float)Math.PI / 2F) + this.bipedHead.rotateAngleX;
-            this.bipedLeftArm.rotateAngleX = -((float)Math.PI / 2F) + this.bipedHead.rotateAngleX;
-        }
-        else if (this.leftArmPose == ModelBiped.ArmPose.BOW_AND_ARROW)
-        {
-            this.bipedRightArm.rotateAngleY = -0.1F + this.bipedHead.rotateAngleY - 0.4F;
-            this.bipedLeftArm.rotateAngleY = 0.1F + this.bipedHead.rotateAngleY;
-            this.bipedRightArm.rotateAngleX = -((float)Math.PI / 2F) + this.bipedHead.rotateAngleX;
-            this.bipedLeftArm.rotateAngleX = -((float)Math.PI / 2F) + this.bipedHead.rotateAngleX;
-        }
-        
-        //boolean zomb = entityIn instanceof EntityZombie && ((EntityZombie)entityIn).isArmsRaised();
-        float z = MathHelper.sin(this.swingProgress * (float)Math.PI);
-        float z1 = MathHelper.sin((1.0F - (1.0F - this.swingProgress) * (1.0F - this.swingProgress)) * (float)Math.PI);
-        this.bipedRightArm.rotateAngleZ = 0.0F;
-        this.bipedLeftArm.rotateAngleZ = 0.0F;
-        this.bipedRightArm.rotateAngleY = -(0.1F - z * 0.6F);
-        this.bipedLeftArm.rotateAngleY = 0.1F - z * 0.6F;
-        float z2 = -(float)Math.PI / (flag ? 1.5F : 2.25F);
-        this.bipedRightArm.rotateAngleX = z2;
-        this.bipedLeftArm.rotateAngleX = z2;
-        this.bipedRightArm.rotateAngleX += z * 1.2F - z1 * 0.4F;
-        this.bipedLeftArm.rotateAngleX += z * 1.2F - z1 * 0.4F;
-        this.bipedRightArm.rotateAngleZ += MathHelper.cos(ageInTicks * 0.09F) * 0.05F + 0.05F;
-        this.bipedLeftArm.rotateAngleZ -= MathHelper.cos(ageInTicks * 0.09F) * 0.05F + 0.05F;
-        this.bipedRightArm.rotateAngleX += MathHelper.sin(ageInTicks * 0.067F) * 0.05F;
-        this.bipedLeftArm.rotateAngleX -= MathHelper.sin(ageInTicks * 0.067F) * 0.05F;
-        
-        copyModelAngles(this.bipedHead, this.bipedHeadwear);
-    }
-    
-    public void setModelAttributes(ModelBase model)
-    {
-        super.setModelAttributes(model);
-    }
-
-    public void setVisible(boolean visible)
-    {
-        this.bipedHead.showModel = visible;
-        this.bipedHeadwear.showModel = visible;
-        this.bipedBody.showModel = visible;
-        this.bipedRightArm.showModel = visible;
-        this.bipedLeftArm.showModel = visible;
-        this.bipedRightLeg.showModel = visible;
-        this.bipedLeftLeg.showModel = visible;
-    }
-
-    public void postRenderArm(float scale, EnumHandSide side)
-    {
-        this.getArmForSide(side).postRender(scale);
-    }
-
-    protected ModelRenderer getArmForSide(EnumHandSide side)
-    {
-        return side == EnumHandSide.LEFT ? this.bipedLeftArm : this.bipedRightArm;
-    }
-
-    protected EnumHandSide getMainHand(Entity entityIn)
-    {
-        if (entityIn instanceof EntityLivingBase)
-        {
-            EntityLivingBase entitylivingbase = (EntityLivingBase)entityIn;
-            EnumHandSide enumhandside = entitylivingbase.getPrimaryHand();
-            return entitylivingbase.swingingHand == EnumHand.MAIN_HAND ? enumhandside : enumhandside.opposite();
-        }
-        else
-        {
-            return EnumHandSide.RIGHT;
-        }
-    }
-
-    @SideOnly(Side.CLIENT)
-    public static enum ArmPose
-    {
-        EMPTY,
-        ITEM,
-        BLOCK,
-        BOW_AND_ARROW;
     }
 }
--- src/main/java/com/sirsquidly/oe/entity/render/RenderPickled.java
+++ src/main/java/com/sirsquidly/oe/entity/render/RenderPickled.java
@@ -5,13 +5,21 @@ import org.lwjgl.opengl.GL11;
 import com.sirsquidly.oe.entity.EntityPickled;
 import com.sirsquidly.oe.entity.model.ModelPickled;
 import com.sirsquidly.oe.entity.render.layer.LayerPickled;
+import com.sirsquidly.oe.init.OEItems;
 import com.sirsquidly.oe.util.Reference;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ModelZombie;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.layers.LayerBipedArmor;
+import net.minecraft.client.renderer.entity.layers.LayerHeldItem;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumHandSide;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.fml.relauncher.Side;
@@ -28,6 +36,60 @@ public class RenderPickled extends RenderLiving<EntityPickled>
     {
 		super(manager, new ModelPickled(), 0.5F);
 		this.addLayer(new LayerPickled(this));
+		this.addLayer(new LayerHeldItem(this)
+        {
+        	public void doRenderLayer(EntityLivingBase entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale)
+            {
+                boolean flag = entitylivingbaseIn.getPrimaryHand() == EnumHandSide.RIGHT;
+                ItemStack itemstack = flag ? entitylivingbaseIn.getHeldItemOffhand() : entitylivingbaseIn.getHeldItemMainhand();
+                ItemStack itemstack1 = flag ? entitylivingbaseIn.getHeldItemMainhand() : entitylivingbaseIn.getHeldItemOffhand();
+
+                if (!itemstack.isEmpty() || !itemstack1.isEmpty())
+                {
+                    GlStateManager.pushMatrix();
+
+                    if (this.livingEntityRenderer.getMainModel().isChild)
+                    {
+                        GlStateManager.translate(0.0F, 0.75F, 0.0F);
+                        GlStateManager.scale(0.5F, 0.5F, 0.5F);
+                    }
+
+                    this.renderHeldItem(entitylivingbaseIn, itemstack1, ItemCameraTransforms.TransformType.THIRD_PERSON_RIGHT_HAND, EnumHandSide.RIGHT);
+                    this.renderHeldItem(entitylivingbaseIn, itemstack, ItemCameraTransforms.TransformType.THIRD_PERSON_LEFT_HAND, EnumHandSide.LEFT);
+                    GlStateManager.popMatrix();
+                }
+            }
+			
+			 private void renderHeldItem(EntityLivingBase entity, ItemStack item, ItemCameraTransforms.TransformType camera, EnumHandSide handSide)
+			    {
+			        if (!item.isEmpty())
+			        {
+			            GlStateManager.pushMatrix();
+			            if (entity.isSneaking())
+			            {
+			                GlStateManager.translate(0.0F, 0.2F, 0.0F);
+			            }
+			            this.translateToHand(handSide);
+			            
+			            if (item.getItem() == OEItems.TRIDENT_ORIG && ((EntityZombie)entity).isArmsRaised())
+			            {
+			            	GlStateManager.rotate(90.0F, 1.0F, 0.0F, 0.0F);
+			            	GlStateManager.translate(-0.15F, 0.0F, 0.0F);
+			            }
+			            else
+			            {
+			            	GlStateManager.rotate(-90.0F, 1.0F, 0.0F, 0.0F);
+				            GlStateManager.rotate(180.0F, 0.0F, 1.0F, 0.0F);
+			            }
+			            boolean flag = handSide == EnumHandSide.LEFT;
+			            GlStateManager.translate((float)(flag ? -1 : 1) / 16.0F, 0.125F, -0.625F);
+			            Minecraft.getMinecraft().getItemRenderer().renderItemSide(entity, item, camera, flag);
+			            GlStateManager.popMatrix();
+			        }
+			    }
+			 
+			 
+        });
         LayerBipedArmor layerbipedarmor = new LayerBipedArmor(this)
         {
             protected void initArmor()
