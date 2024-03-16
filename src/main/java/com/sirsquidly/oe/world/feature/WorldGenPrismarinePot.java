9d2e2ad8930a1bee606d3e48c3df83b9cae76b52
Sir Squidly <sir.squildy.le.third@gmail.com>
2024-03-16 15:26:48 -0700

    Prismarine Pots are BACK
    + Re-added Prismarine Pot Generation

--- src/main/java/com/sirsquidly/oe/world/feature/WorldGenPrismarinePot.java
+++ src/main/java/com/sirsquidly/oe/world/feature/WorldGenPrismarinePot.java
@@ -2,20 +2,11 @@ package com.sirsquidly.oe.world.feature;
 
 import java.util.Random;
 
-import com.sirsquidly.oe.blocks.BlockCoral;
-import com.sirsquidly.oe.blocks.BlockCoralFan;
-import com.sirsquidly.oe.blocks.BlockCoralFull;
-import com.sirsquidly.oe.blocks.BlockSeagrasss;
-import com.sirsquidly.oe.blocks.BlockDulse;
-import com.sirsquidly.oe.blocks.BlockSeaPickle;
-import com.sirsquidly.oe.blocks.BlockTubeSponge;
-import com.sirsquidly.oe.blocks.IChecksWater;
-import com.sirsquidly.oe.init.OEBlocks;
-import com.sirsquidly.oe.util.handlers.ConfigHandler;
-
-import net.minecraft.block.Block;
+import net.minecraft.block.BlockPrismarine;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
@@ -24,15 +15,17 @@ import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraftforge.fml.common.IWorldGenerator;
 
+import com.sirsquidly.oe.blocks.BlockPrismarinePot;
+import com.sirsquidly.oe.init.OEBlocks;
+import com.sirsquidly.oe.tileentity.TilePrismarinePot;
+import com.sirsquidly.oe.util.handlers.LootTableHandler;
+
 /**
- * Used for generating patches of plants/blocks on the ocean floor(EX. Seagrass or Tube Sponge).
+ * Generates patches of Prismarine Pots.
  */
-public class WorldGenOceanPatch implements IWorldGenerator
+public class WorldGenPrismarinePot implements IWorldGenerator
 {
-	private final Block block;
 	private int patchAmount;
-	/** Currently exclusive for Seagrass. Float chance (0.0 - 1.0) for generating Double/Tall Seagrass. Higher is more likely.*/
-	private double tallChance;
 	private int attemptsPerChunk;
 	private int chancePerAttempt;
 	/** Flips the generator to run from the world bottom up, always failing if the placed Seagrass has sky access. For underground / beneath structure generation.*/
@@ -44,20 +37,18 @@ public class WorldGenOceanPatch implements IWorldGenerator
 	private Biome[] biomes;
 
 	/** Simplified because not every use needs to specify the spread or seagrass height chance.*/
-	public WorldGenOceanPatch(Block blockIn, int perChunk, int perAttempt, int amount, boolean rising, Biome... biomes)
+	public WorldGenPrismarinePot(int perChunk, int perAttempt, int amount, boolean rising, Biome... biomes)
 	{
-		this(blockIn, perChunk, perAttempt, amount, 8, 4, 0.0, rising, biomes);
+		this(perChunk, perAttempt, amount, 8, 4, rising, biomes);
 	}
 	
-	public WorldGenOceanPatch(Block blockIn, int perChunk, int perAttempt, int amount, int spreadXZ, int spreadY, double tall, boolean rising, Biome... biomes)
+	public WorldGenPrismarinePot(int perChunk, int perAttempt, int amount, int spreadXZ, int spreadY, boolean rising, Biome... biomes)
 	{
-		this.block = blockIn;
 		this.attemptsPerChunk = perChunk; 
 		this.chancePerAttempt = perAttempt; 
 		this.patchAmount = amount;
 		this.placeSpreadXZ = spreadXZ;
 		this.placeSpreadY = spreadY;
-		this.tallChance = tall;
 		this.bottomUp = rising;
 		this.biomes = biomes;
 	}
@@ -98,7 +89,7 @@ public class WorldGenOceanPatch implements IWorldGenerator
 						{ 
 							pos = pos.up();
 							
-							if(this.block.canPlaceBlockAt(world, pos.down()))
+							if(world.getBlockState(pos.down()).getBlock() == Blocks.WATER && world.getBlockState(pos).isSideSolid(world, pos, EnumFacing.UP))
 							{ beginPatchPlacing(world, rand, pos); break;}
 						}
 					}
@@ -107,7 +98,7 @@ public class WorldGenOceanPatch implements IWorldGenerator
 						for ( IBlockState state = world.getBlockState(pos); (state.getBlock().isReplaceable(world, pos) && pos.getY() > 0); state = world.getBlockState(pos) )
 			        	{ pos = pos.down(); }
 						
-						if(this.block.canPlaceBlockAt(world, pos.up()))
+						if(world.getBlockState(pos.up()).getBlock() == Blocks.WATER && world.getBlockState(pos).isSideSolid(world, pos, EnumFacing.UP))
 						{ beginPatchPlacing(world, rand, pos); }
 					}
 				}
@@ -125,7 +116,7 @@ public class WorldGenOceanPatch implements IWorldGenerator
         	
             BlockPos blockpos = position.add(rX, rand.nextInt(placeSpreadY) - rand.nextInt(placeSpreadY), rZ);
             
-            if (worldIn.getBlockState(blockpos).getBlock() == Blocks.WATER && this.block.canPlaceBlockAt(worldIn, blockpos))
+            if (worldIn.getBlockState(blockpos).getBlock() == Blocks.WATER && worldIn.getBlockState(blockpos.down()).isSideSolid(worldIn, blockpos.down(), EnumFacing.UP))
             {
             	placeBlock(worldIn, rand, blockpos);
             }
@@ -136,44 +127,18 @@ public class WorldGenOceanPatch implements IWorldGenerator
     /** Used for placing the individual blocks because many blocks require specific checks/randomizing. **/
     public void placeBlock(World worldIn, Random rand, BlockPos pos)
     {
-    	if (this.block == OEBlocks.SEAGRASS && ConfigHandler.block.seagrass.enableTallSeagrass && rand.nextDouble() < tallChance && OEBlocks.SEAGRASS.canPlaceBlockAt(worldIn, pos) && ((BlockSeagrasss) OEBlocks.SEAGRASS).checkPlaceWater(worldIn, pos.up(), true))
-    	{
-    		((BlockSeagrasss) OEBlocks.SEAGRASS).placeAt(worldIn, pos, 16 | 2);
-    	}
-    	else if (this.block == OEBlocks.SEA_PICKLE)
-    	{
-    		if (pos.getY() < worldIn.getSeaLevel() - 1) worldIn.setBlockState(pos, this.block.getDefaultState().withProperty(BlockSeaPickle.AMOUNT, rand.nextInt(4) + 1), 2);
-    	}
-    	else if (this.block == OEBlocks.DULSE)
-    	{
-    		if (pos.getY() < worldIn.getSeaLevel() - 1)
-    		{
-    			int dulseAge = rand.nextInt(4);
-    			
-    			if (dulseAge >= 3 && ((IChecksWater) OEBlocks.DULSE).checkWater(worldIn, pos.up()))
-    			{
-    				((BlockDulse) OEBlocks.DULSE).placeAt(worldIn, pos, 16 | 2);
-    			}
-    			if (dulseAge < 3)
-    			{
-    				worldIn.setBlockState(pos, this.block.getDefaultState().withProperty(BlockDulse.AGE, dulseAge), 2);
-    			}
-    		}
-    	}
-    	else if (this.block == OEBlocks.TUBE_SPONGE)
-    	{
-    		if (pos.getY() < worldIn.getSeaLevel() - 10) worldIn.setBlockState(pos, this.block.getDefaultState().withProperty(BlockTubeSponge.AGE, rand.nextInt(3)), 2);
-    	}
-    	else if (this.block instanceof BlockCoralFan)
-    	{
-    		if (pos.getY() < worldIn.getSeaLevel() - 10) ((BlockCoralFan) OEBlocks.BLUE_CORAL_FAN).placeAt(worldIn, pos, rand, this.block, true);
-    	}
-    	else if (this.block instanceof BlockCoral)
-    	{
-    		if (pos.getY() < worldIn.getSeaLevel() - 10 && worldIn.getBlockState(pos.down()).getBlock() instanceof BlockCoralFull) 
-    		{ worldIn.setBlockState(pos, this.block.getDefaultState().withProperty(BlockCoral.IN_WATER, true), 16 | 2); }
-    	}
-    	else
-    	{ worldIn.setBlockState(pos, this.block.getDefaultState(), 16 | 2); }
-    }
+		if (worldIn.getBlockState(pos.down()).getBlock() instanceof BlockPrismarine)
+		{
+			BlockPrismarinePot.EnumAxis randPotRotation = rand.nextInt(2) == 0 ? BlockPrismarinePot.EnumAxis.X : BlockPrismarinePot.EnumAxis.Z;
+    		
+    		worldIn.setBlockState(pos, OEBlocks.PRISMARINE_POT.getDefaultState().withProperty(BlockPrismarinePot.FACING, randPotRotation).withProperty(BlockPrismarinePot.SEALED, rand.nextInt(10) == 0), 3);
+    		
+            TileEntity tileentity = worldIn.getTileEntity(pos);
+
+            if (tileentity instanceof TilePrismarinePot)
+            {
+                ((TilePrismarinePot)tileentity).setLootTable(LootTableHandler.MONUMENT_MYSTIC, rand.nextLong());
+            }
+		}
+	}
 }
